ef1c0c5ff533d2f35fc909cab522ac875a030177
Baptiste Legat <legatbaptiste@gmail.com>
2024-10-17 15:43:43 +0200

    clear code

--- public/assets/@symfony/stimulus-bundle/controllers-6415d53ff2bedd072b2db04d892f27ee.js
+++
@@ -1,7 +0,0 @@
-import controller_0 from "../ux-live-component/live_controller.js";
-import "../ux-live-component/live.min.css";
-import controller_1 from "../ux-turbo/turbo_controller.js";
-import controller_2 from "../../controllers/hello_controller.js";
-export const eagerControllers = {"live": controller_0, "symfony--ux-turbo--turbo-core": controller_1, "hello": controller_2};
-export const lazyControllers = {};
-export const isApplicationDebug = true;
