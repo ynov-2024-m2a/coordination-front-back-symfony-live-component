60d01f7abd11c645be994e6be72f343a6b827445
IlyesDEO <114987975+IlyesDEO@users.noreply.github.com>
2024-10-17 14:20:01 +0200

    Delete vendor directory

--- vendor/symfony/ux-live-component/assets/dist/Component/plugins/LazyPlugin.d.ts
+++
@@ -1,7 +0,0 @@
-import type { PluginInterface } from './PluginInterface';
-import type Component from '../index';
-export default class implements PluginInterface {
-    private intersectionObserver;
-    attachToComponent(component: Component): void;
-    private getObserver;
-}
