87ac5f55ef3b526996b5a3fb242655dba412fa65
Ilyes-DEO <ide.dno.lyon@gmail.com>
2024-11-04 14:25:21 +0100

    Ajout du component docslink

---
+++ src/Service/DocsLink.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
+
+#[AsTwigComponent]
+class DocsLink
+{
+    public string $url;
+    public string $title;
+    public string $text;
+
+    public ?string $icon = null;
+
+    #[ExposeInTemplate]
+    public function isExternal(): bool
+    {
+        return !str_starts_with($this->url, 'https://symfony.com');
+    }
+}
