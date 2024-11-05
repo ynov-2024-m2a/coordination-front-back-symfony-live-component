164a8510abadbe641b20330b0811a1397d181a4d
Baptiste Legat <legatbaptiste@gmail.com>
2024-11-05 10:19:02 +0100

    clean projet

--- migrations/Version20241105091137.php
+++ migrations/Version20241105091137.php
@@ -14,7 +14,7 @@ final class Version20241105091137 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add animal and messenger_messages tables';
     }
 
     public function up(Schema $schema): void
